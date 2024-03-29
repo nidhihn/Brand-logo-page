d409ca12150d523736830121c21c4c5d111f18fa
Nidhi H N <nidhinshetty28@gmail.com>
2024-03-29 18:55:15 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Herosection from "./components/Hero";
+import Navigation from "./components/Navigation";
+const App= () =>{
+    return(
+        <div>
+          <Navigation/>
+          <Herosection/> 
+        </div>
+    );
+};
+
+
+
+export default App;
